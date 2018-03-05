ed843a56bb6f794db721d1d4d89e6481987d727b
Iza Dziura <izadziura@wp.pl>
2018-03-05 19:30:33 +0100

    Add comment to the js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 // scripts.js file
-
+//alert('test');
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+//Komentarz liniowy
+
+/* A może
+	lepiej
+blokowy? */
